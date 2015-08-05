0a1e9bcf6f7d6bc8ce01dc44b53304507f0210fe
arter97 <qkrwngud825@gmail.com>
2015-08-05 21:38:54 +0000

    msm_adreno_tz: be aware of suspended state
    Force to use & record as min freq when system has
    entered pm-suspend or screen-off state.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -1,4 +1,3 @@
-
 /* Copyright (c) 2010-2014,2016 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -23,6 +22,7 @@
 #include <linux/msm_adreno_devfreq.h>
 #include <asm/cacheflush.h>
 #include <soc/qcom/scm.h>
+#include <linux/powersuspend.h>
 #include "governor.h"
 
 #ifdef CONFIG_ADRENO_IDLER
@@ -66,6 +66,8 @@ static void do_partner_start_event(struct work_struct *work);
 static void do_partner_stop_event(struct work_struct *work);
 static void do_partner_suspend_event(struct work_struct *work);
 static void do_partner_resume_event(struct work_struct *work);
+/* Boolean to detect if pm has entered suspend mode */
+static bool suspended = false;
 
 static struct workqueue_struct *workqueue;
 
@@ -204,6 +206,17 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 	}
 
 	*freq = stats.current_frequency;
+	*flag = 0;
+
+	/*
+	 * Force to use & record as min freq when system has
+	 * entered pm-suspend or screen-off state.
+	 */
+	if (suspended || power_suspended) {
+		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
+		return 0;
+	}
+
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
 
@@ -355,6 +368,8 @@ static int tz_resume(struct devfreq *devfreq)
 	struct devfreq_dev_profile *profile = devfreq->profile;
 	unsigned long freq;
 
+	suspended = false;
+
 	freq = profile->initial_freq;
 
 	return profile->target(devfreq->dev.parent, &freq, 0);
@@ -363,6 +378,7 @@ static int tz_resume(struct devfreq *devfreq)
 static int tz_suspend(struct devfreq *devfreq)
 {
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
+
 	#ifdef CONFIG_ADRENO_IDLER
 	if (adreno_idler_active == false)
 	{
