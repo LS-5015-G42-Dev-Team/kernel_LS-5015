09c82f4a9f0f855c8b4547307ff8c2e30c94eb42
Joe Maples <joe@frap129.org>
2016-12-20 19:14:10 +0000

    adreno_idler: Add display state awareness
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -45,7 +45,7 @@
    Any workload higher than this will be treated as a non-idle workload.
    Adreno idler will more actively try to ramp down the frequency
    if this is set to a higher value. */
-static unsigned long idleworkload = 5000;
+static unsigned long idleworkload = 7000;
 module_param_named(adreno_idler_idleworkload, idleworkload, ulong, 0664);
 
 /* Number of events to wait before ramping down the frequency.
@@ -73,6 +73,9 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 	if (!adreno_idler_active)
 		return 0;
 
+	/* Boolean to let us know if the display is on*/
+	bool display_on = is_display_on();
+
 	if (stats.busy_time < idleworkload) {
 		/* busy_time >= idleworkload should be considered as a non-idle workload. */
 		idlecount++;
@@ -88,7 +91,7 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			idlecount--;
 			return 1;
 		}
-	} else if (state_suspended) {
+	} else if (!display_on) {
 		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 1;
