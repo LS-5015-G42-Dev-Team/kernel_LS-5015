95b74b689cb2153f3f57f9c86b2624bd02bc7876
Shekhawat2 <anandsingh215@yahoo.com>
2017-10-08 12:06:53 +0530

    msm: thermal: simple: Use msm8916 core mask
    Signed-off-by: Shekhawat2 <anandsingh215@yahoo.com>

--- drivers/thermal/msm_thermal_simple.c
+++ drivers/thermal/msm_thermal_simple.c
@@ -29,9 +29,9 @@
 #include <linux/qpnp/qpnp-adc.h>
 #include <linux/slab.h>
 
-/* For MSM8996 */
-#define LITTLE_CPU_ID	0
-#define BIG_CPU_ID	2
+/* For MSM8916 */
+#define LITTLE_CPU_ID	4
+#define BIG_CPU_ID	0
 
 #define DEFAULT_SAMPLING_MS 3000
 
