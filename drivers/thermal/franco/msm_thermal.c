08d7c56a1daf3f3285d4f0db7e47b573d3e62d4d
Shoaib0597 <Shoaib0595@gmail.com>
2017-10-16 17:12:20 +0530

    Franco's Thermal: Introduce Shoaib's Core Control
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    Although, I got Franco's Thermal Driver to work, there was still one thing missing in the Thermal Department and that was, a fully Automated, Temperature-Based, Core-Control Driver. Hence, I started working on this too. I paid proper attention to the mechanism of HotPlugging Cores and after many hours of tweaking and testing, I finally managed to develop an Efficient way to HotPlug Cores based on Temperature. After I got my Core-Control Driver working, I merged this Driver of mine into Franco's Thermal Driver's Source Code to keep things tidy and clean. This Driver is now called as Shoaib's Core Control.
    
    The Core-Disabling Mechanism of Shoaib's Core-Control is explained below---
    For big.LITTLE Octa-Core SoCs---
    Temperature (Celsius)   Cores (Numbering starts from 0)
    at or below 45		All Cores Online
    between 46 and 48       Safe-Difference of 2 Celsius
    between 49 and 59       Disable Cores 2 and 3
    between 60 and 80	Disable the big Cluster except Core 0
    at or above 81		Disable Cores 1, 2, 3, 6 and 7

--- drivers/thermal/franco/msm_thermal.c
+++ drivers/thermal/franco/msm_thermal.c
@@ -29,6 +29,10 @@
 unsigned int temp_threshold = 65;
 module_param(temp_threshold, int, 0644);
 
+// Core Control sysFS Toggle.
+bool core_control = true;
+static struct kobject *cc_kobj;
+
 uint32_t Throttle_Freq = 800000;
 
 static struct thermal_info {
@@ -93,7 +97,7 @@ static void limit_cpu_freqs(uint32_t max_freq)
 	info.pending_change = false;
 }
 
-static void check_temp(struct work_struct *work)
+static void __ref check_temp(struct work_struct *work)
 {
 	struct tsens_device tsens_dev;
 	uint32_t freq = 0;
@@ -129,9 +133,157 @@ static void check_temp(struct work_struct *work)
 			info.throttling = true;
 	}
 
+	// Begin HotPlug Mechanism for Shoaib's Core Control.
+	if (core_control)
+	{
+	   if (temp >= 81)
+	   {
+	      // Enable Core 0 (big).
+	      if (!cpu_online(0))
+		 cpu_up(0);
+
+	      // Disable Cores (big).
+	      if (cpu_online(3))
+	      	 cpu_down(3);
+	      if (cpu_online(2))
+	         cpu_down(2);
+	      if (cpu_online(1))
+	         cpu_down(1);
+
+	      // Disable Cores (LITTLE).
+	      if (cpu_online(7))
+	         cpu_down(7);
+	      if (cpu_online(6))
+	         cpu_down(6); 
+	   }
+	   else if (temp >= 60 && temp <= 80)
+	   {
+		   // Enable Cores (LITTLE).
+		   if (!cpu_online(6))
+		      cpu_up(6);
+	           if (!cpu_online(7))
+	 	      cpu_up(7);
+
+		   // Disable Cores (big).
+	           if (cpu_online(3))
+	      	      cpu_down(3);
+	           if (cpu_online(2))
+	              cpu_down(2);
+		   if (cpu_online(1))
+	              cpu_down(1);
+	   }
+	   else if (temp >= 49 && temp <= 59)
+	   {
+		   // Enable Cores (big).
+	           if (!cpu_online(0))
+	              cpu_up(0);
+	           if (!cpu_online(1))
+	              cpu_up(1);
+	 
+		   // Disable Cores (big).         
+	           if (cpu_online(3))
+	              cpu_down(3);
+	           if (cpu_online(2))
+	              cpu_down(2);
+	   }
+	   else if (temp == 45)
+	   {
+	           int cpu;
+	           for_each_possible_cpu(cpu)
+	               if (!cpu_online(cpu))
+		          cpu_up(cpu);
+	   }
+	}
+	// End HotPlug Mechanism for Shoaib's Core Control.
+
 reschedule:
 	schedule_delayed_work_on(0, &check_temp_work, msecs_to_jiffies(250));
 }
+
+// Begin sysFS for Shoaib's Core Control.
+static ssize_t show_cc_enabled (struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return snprintf (buf, PAGE_SIZE, "%d\n", core_control);
+}
+
+static ssize_t __ref store_cc_enabled (struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int ret = 0;
+	int val = 0;
+
+	ret = kstrtoint (buf, 10, &val);
+	if (ret)
+	{
+	   pr_err("Invalid input %s. err:%d\n", buf, ret);
+	   goto done_store_cc;
+	}
+
+	if (core_control == !!val)
+	   goto done_store_cc;
+
+	core_control = !!val;
+
+	if (!core_control)
+	{
+	   int cpu;
+	   /* Wake-Up All the Sibling Cores */
+	   for_each_possible_cpu(cpu)
+	       if (!cpu_online(cpu))
+		  cpu_up(cpu);
+	}
+
+done_store_cc:
+	return count;
+}
+
+static __refdata struct kobj_attribute cc_enabled_attr = 
+__ATTR(core_control, 0644, show_cc_enabled, store_cc_enabled);
+
+static __refdata struct attribute *cc_attrs[] = {
+	&cc_enabled_attr.attr,
+	NULL,
+};
+
+static __refdata struct attribute_group cc_attr_group = {
+	.attrs = cc_attrs,
+};
+
+static __init int msm_thermal_add_cc_nodes(void)
+{
+	struct kobject *module_kobj = NULL;
+	int ret = 0;
+
+	module_kobj = kset_find_obj(module_kset, KBUILD_MODNAME);
+	if (!module_kobj) 
+	{
+	   pr_err("cannot find kobject\n");
+	   ret = -ENOENT;
+	   goto done_cc_nodes;
+	}
+
+	cc_kobj = kobject_create_and_add("core_control", module_kobj);
+	if (!cc_kobj) 
+	{
+	   pr_err("cannot create core control kobj\n");
+	   ret = -ENOMEM;
+	   goto done_cc_nodes;
+	}
+
+	ret = sysfs_create_group(cc_kobj, &cc_attr_group);
+	if (ret) 
+	{
+	   pr_err("cannot create sysfs group. err:%d\n", ret);
+	   goto done_cc_nodes;
+	}
+
+	return 0;
+
+done_cc_nodes:
+	if (cc_kobj)
+	   kobject_del(cc_kobj);
+	return ret;
+}
+// End sysFS for Shoaib's Core Control.
 
 static int msm_thermal_dev_probe(struct platform_device *pdev)
 {
@@ -182,6 +334,10 @@ static struct platform_driver msm_thermal_device_driver = {
 
 int __init  msm_thermal_device_init(void)
 {
+	// Initialize Shoaib's Core Control Driver only for Octa-Core SoCs.
+	if (num_possible_cpus() == 8)
+	   msm_thermal_add_cc_nodes();
+
 	return platform_driver_register(&msm_thermal_device_driver);
 }
 
